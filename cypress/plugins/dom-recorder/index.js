9e7addcfffee7d52f955e73a2891f3d7adc3a51e
kabinja <renaudrwemalika@gmail.com>
2022-04-27 19:16:26 +0200

    start working on ast traversal

--- cypress/plugins/dom-recorder/index.js
+++ cypress/plugins/dom-recorder/index.js
@@ -1,11 +1,48 @@
 const fs = require("fs");
-const { parseScript } = require("shift-parser");
+
+function getContent(filePath) {
+  return fs.readFileSync(filePath, { encoding: "utf8", flag: "r" });
+}
+
+function traverseAst(ast, locations) {
+  const {traverse} = require("shift-traverser");
+
+  traverse(ast, {
+    enter(node) {
+      if(node.type == 'CallExpression'){
+        if(node.callee.type == 'IdentifierExpression' && node.callee.name == 'describe'){
+            console.log(`test name: ${node.arguments[0].value}`)
+        }
+
+      }
+    },
+  });
+}
+
+function instrument(content) {
+  const {
+    parseScriptWithLocation,
+    parseModuleWithLocation,
+  } = require("shift-parser");
+
+  try {
+    const { tree, locations } = parseScriptWithLocation(content);
+    traverseAst(tree, locations);
+    return content;
+  } catch (err) {
+    const { tree, locations } = parseModuleWithLocation(content);
+    traverseAst(tree, locations);
+    return "";
+  }
+}
 
 function domRecorder(file) {
-  const { filePath, outputPath, shouldWatch } = file;
-  console.log({ filePath, outputPath, shouldWatch });
-  const ast = parseScript(filePath);
-  fs.writeFileSync(outputPath, "", "utf8");
+  const { filePath, outputPath } = file;
+  const content = getContent(filePath);
+  const instrumented = instrument(content);
+
+  fs.writeFileSync(outputPath, instrumented, "utf8");
+
   return outputPath;
 }
 
